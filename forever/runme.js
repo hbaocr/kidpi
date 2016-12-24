b2fa107b0887b73cfcfeb5bbd273b611b58f9997
Matthew Amacker <mamacker@google.com>
2016-12-24 18:33:41 +0000

    Able to act on outlets based on remote.

--- forever/runme.js
+++ forever/runme.js
@@ -2,9 +2,6 @@
 var child_process = require('child_process');
 
 console.log("#################################### I started!");
-setInterval(function() {
-  console.log("I'm alive!!! " + process.pid)
-},1000)
 
 setInterval(function() {
   var command = "/bin/systemd-notify --pid="+process.pid+" WATCHDOG=1";
@@ -15,4 +12,24 @@ setInterval(function() {
     'WATCHDOG=1'
   ]
   child_process.execFile('/bin/systemd-notify', args);
-},3000);
+  //child_process.execFile(command);
+},10000);
+
+var net = require('net');
+var Hs100Api = require('hs100-api');
+var last = true;
+
+// This server listens on a Unix socket at /var/run/mysocket
+var unixServer = net.createServer(function(client) {
+  console.log("Received connection!");
+  
+  var client = new Hs100Api.Client();
+  var lightplug = client.getPlug({host: '192.168.1.107'});
+  var fanplug = client.getPlug({host: '192.168.1.177'});
+  var mattplug = client.getPlug({host: '192.168.1.164'});
+  
+  last = !last;
+  lightplug.setPowerState(last);
+  mattplug.setPowerState(last);
+});
+unixServer.listen('/var/run/lirc/lircrun');
