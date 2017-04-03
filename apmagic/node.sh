4a857b189c31b145227e4d7f9e63182a7a536488
Matthew Amacker <mamacker@google.com>
2017-04-03 04:32:59 +0000

    Checkin of node install script.

---
+++ apmagic/node.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+wget https://nodejs.org/dist/v6.10.1/node-v6.10.1-linux-armv6l.tar.xz
+sleep 5s
+echo "Done pulling tar."
+
+tar xvf node-v6.10.1-linux-armv6l.tar.xz
+
+sleep 5s
+echo "Done untar."
+
+cd node-v6.10.1-linux-armv6l
+sudo cp -R bin/* /usr/bin/
+
+sleep 5s
+echo "Done copy"
+sudo cp -R lib/* /usr/lib/
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install build-essential
