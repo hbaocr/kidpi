c04cee6100e7bfc3db25be10347ed99aadeede06
Matthew Amacker <mamacker@google.com>
2017-04-26 18:41:47 +0000

    Complete system, that comes up on reboot.

--- apmagic/index.js
+++ apmagic/index.js
@@ -76,17 +76,19 @@ function dnsmasq(options, callback) {
     }
   });
 
-  tmp.file((err, path, fd) => {
-    if (err) throw err;
+  exec('/etc/init.d/dnsmasq stop', () => {
+    tmp.file((err, path, fd) => {
+      if (err) throw err;
 
-    console.log('File: ', path);
-    fs.write(fd, commands.join('\n'));
+      console.log('File: ', path);
+      fs.write(fd, commands.join('\n'));
 
-    console.log("Commands being executed: ", commands);
-    exec('dnsmasq -C ' + path);
-    if (callback) {
-      callback();
-    }
+      console.log("Commands being executed: ", commands);
+      exec('dnsmasq -C ' + path);
+      if (callback) {
+        callback();
+      }
+    });
   });
 }
 
@@ -97,41 +99,52 @@ function trafficForwarding(callback) {
     fs.write(fd, '1');
     console.log('ip_forward flag written.');
 
-    iptables('eth0', '-t nat -A POSTROUTING -j MASQUERADE');
-    iptables('wlan0', '-t nat -A FORWARD -i eth0 -m state --start RELATED,ESTABLISHED -j ACCEPT');
-    iptables('eth0', '-A FORWARD -i wlan0 -j ACCEPT', callback);
+    // Note, iptables does not like being blated with several rules instantaneously.
+    // So I used the event emitter to organize them.
+    iptables('eth0', '-t nat -A POSTROUTING -j MASQUERADE', () => {myEmitter.emit('eth0setup')});
+
+    myEmitter.on('eth0setup', () => {
+      iptables('wlan0', '-t nat -A FORWARD -i eth0 -m state --start RELATED,ESTABLISHED -j ACCEPT');
+      myEmitter.emit('forwardingSetup');
+    });
+
+    myEmitter.on('forwardingSetup', () => {
+      iptables('eth0', '-A FORWARD -i wlan0 -j ACCEPT', callback);
+    });
   });
 }
   
-  
+function createAccessPoint() {  
+  takeDown('wlan0', (err) => {
+    console.log("Trying to bring down wlan.  Err?", err);
+    myEmitter.emit('down');
+  });
 
-takeDown('wlan0', (err) => {
-  console.log("Trying to bring down wlan.  Err?", err);
-  myEmitter.emit('down');
-});
-
-myEmitter.on('down', () => {
-  ifconfig('wlan0', 'inet', '172.24.1.1', (err) => {
-    console.log("Err setting the ip address?", err);
-    ifconfig('wlan0', 'broadcast', '172.24.1.255', (err) => {
-      console.log("Err setting the broadcast address?", err);
-      ifconfig('wlan0', 'netmask', '255.255.255.0', (err) => {
-        console.log("Err setting the mask address?", err);
-        myEmitter.emit('ipconfigured');
+  myEmitter.on('down', () => {
+    ifconfig('wlan0', 'inet', '172.24.1.1', (err) => {
+      console.log("Err setting the ip address?", err);
+      ifconfig('wlan0', 'broadcast', '172.24.1.255', (err) => {
+        console.log("Err setting the broadcast address?", err);
+        ifconfig('wlan0', 'netmask', '255.255.255.0', (err) => {
+          console.log("Err setting the mask address?", err);
+          myEmitter.emit('ipconfigured');
+        });
       });
     });
   });
-});
 
-myEmitter.on('ipconfigured', () => {
-  hostapd({}, () => {
-    console.log("Done setting up hostap.");
-    dnsmasq({}, () => {
-      console.log("Done setting up dnsmasq.");
-      trafficForwarding(() => {
-        console.log("Traffic forwarding setup.");
+  myEmitter.on('ipconfigured', () => {
+    hostapd({}, () => {
+      console.log("Done setting up hostap.");
+      dnsmasq({}, () => {
+        console.log("Done setting up dnsmasq.");
+        trafficForwarding(() => {
+          console.log("Traffic forwarding setup.");
+        });
       });
     });
   });
-});
+}
+
+setTimeout(createAccessPoint, 30000);
 
