ca6ee6e654fcaa96e1de619329f6b59f1881eefc
Matthew Amacker <matt@soovle.com>
2017-05-12 07:53:55 -0700

    Continued refactoring...

--- apmagic/index.js
+++ apmagic/index.js
@@ -32,117 +32,10 @@ var exec = child_process.exec;
 var fs = require('fs');
 var tmp = require('tmp');
 
+const APExecs = require('./apexecs.js');
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
-
-// Simple function for using the command line for taking down an interface.
-function takeDown(interface, callback) {
-  exec('ifdown ' + interface, callback);
-}
-
-// Simplified function for making use of ifconfig command line tool.
-//ifconfig -v wlan0 inet 172.24.1.1
-//ifconfig -v wlan0 broadcast 172.24.1.255
-function ifconfig(interface, addressType, address, callback) {
-  exec('ifconfig ' + interface + " "+ addressType + " " + address, callback);
-}
-
-// Simplified interface to iptables.
-function iptables(interface, flagString, callback) {
-  var command = 'iptables -o ' + interface + " " + flagString;
-  exec(command, callback);
-}
-
-// Function for making hostapd available to nodejs.  Has basic options
-// for the AP, but also allows for pass-in configuration parameters.
-function hostapd(options, callback) {
-  var commands = [];
-
-  // Known good options for the Raspberry PI 3.  If you are using the 
-  // Raspberry PI Zero the driver value might need to be different.
-  var defaultOptions = {
-    driver:'nl80211', // <--- Make sure this is right for your hardware.
-    channel:6,
-    hw_mode:'g',
-    interface:'wlan0',
-    ssid:'MattWuzHere', // <--- This is going to be the name of the AP
-    wpa:'2',
-    wpa_passphrase:'easypeazie' // <--- This is going to be the AP's password.
-  }
-
-  const finalOptions = Object.assign(options, defaultOptions);
- 
-  Object.getOwnPropertyNames(finalOptions).forEach(function(key) {
-    commands.push(key + '=' + options[key]);
-  });
-
-  // The tmp package does nice things for you, like creating a tmp file in the proper
-  // location and making sure its deleted after the fact.  Hostapd really wants to
-  // take its configurations as a file.  So we shove all the options into one and 
-  // pass it along.
-  tmp.file((err, path, fd) => {
-    if (err) throw err;
-
-    // In case you want to look at the config file:
-    console.log('File: ', path);
-
-    // We then write in the configurations...
-    fs.write(fd, commands.join('\n'));
-
-    console.log("Commands being executed: ", commands);
-
-    // Then execute the hostapd with the file and boom - AP should be started.
-    exec('hostapd ' + path);
-
-    // Now that we are done - go ahead and let others know.
-    if (callback) {
-      callback();
-    }
-  });
-}
-
-
-// Simplified access to dnsmasq - the fellow responsible for handing out IP
-// addresses to your wifi clients.  This can take commands as parameters
-// but this function again takes advantage of the file configuration method.
-function dnsmasq(options, callback) {
-  var commands = [];
-  var defaultOptions = {
-    'interface':'wlan0',
-    'listen-address':'172.24.1.1',
-    'bind-interfaces':'',
-    'server': '8.8.8.8', // <--- Google's DNS servers.  Very handy.
-    'domain-needed':'',
-    'bogus-priv':'',
-    'dhcp-range':'172.24.1.50,172.24.1.150,12h'
-  }
-
-  const finalOptions = Object.assign(options, defaultOptions);
-
-  Object.getOwnPropertyNames(finalOptions).forEach(function(key) {
-    if (options[key] != '') {
-      commands.push(key + '=' + options[key]);
-    } else {
-      commands.push(key);
-    }
-  });
-
-  exec('/etc/init.d/dnsmasq stop', () => {
-    tmp.file((err, path, fd) => {
-      if (err) throw err;
-
-      console.log('File: ', path);
-      fs.write(fd, commands.join('\n'));
-
-      console.log("Commands being executed: ", commands);
-      exec('dnsmasq -C ' + path);
-      if (callback) {
-        callback();
-      }
-    });
-  });
-}
 
 // This uses the temporary flag setting that is possible by writting to the pseudo file
 // ip_forward.  There are more permanent ways.
@@ -155,17 +48,17 @@ function trafficForwarding(callback) {
 
     // Note, iptables does not like being blated with several rules instantaneously.
     // So I used the event emitter to organize them.
-    iptables('eth0', '-t nat -A POSTROUTING -j MASQUERADE', () => {myEmitter.emit('eth0setup')});
+    APExecs.iptables('eth0', '-t nat -A POSTROUTING -j MASQUERADE', () => {myEmitter.emit('eth0setup')});
 
     // Takes care of forwarding the traffic from wlan0 to eth0
     myEmitter.on('eth0setup', () => {
-      iptables('wlan0', '-t nat -A FORWARD -i eth0 -m state --start RELATED,ESTABLISHED -j ACCEPT');
+      APExecs.iptables('wlan0', '-t nat -A FORWARD -i eth0 -m state --start RELATED,ESTABLISHED -j ACCEPT');
       myEmitter.emit('forwardingSetup');
     });
 
     // Makes sure the responses coming to eth0 make it through to wlan0
     myEmitter.on('forwardingSetup', () => {
-      iptables('eth0', '-A FORWARD -i wlan0 -j ACCEPT', callback);
+      APExecs.iptables('eth0', '-A FORWARD -i wlan0 -j ACCEPT', callback);
     });
   });
 }
@@ -174,17 +67,17 @@ function trafficForwarding(callback) {
 // callbacks and events.  Events help you pop the stack on
 // deep callback hell.
 function createAccessPoint() {  
-  takeDown('wlan0', (err) => {
+  APExecs.takeDown('wlan0', (err) => {
     console.log("Trying to bring down wlan.  Err?", err);
     myEmitter.emit('down');
   });
 
   myEmitter.on('down', () => {
-    ifconfig('wlan0', 'inet', '172.24.1.1', (err) => {
+    APExecs.ifconfig('wlan0', 'inet', '172.24.1.1', (err) => {
       console.log("Err setting the ip address?", err);
-      ifconfig('wlan0', 'broadcast', '172.24.1.255', (err) => {
+      APExecs.ifconfig('wlan0', 'broadcast', '172.24.1.255', (err) => {
         console.log("Err setting the broadcast address?", err);
-        ifconfig('wlan0', 'netmask', '255.255.255.0', (err) => {
+        APExecs.ifconfig('wlan0', 'netmask', '255.255.255.0', (err) => {
           console.log("Err setting the mask address?", err);
           myEmitter.emit('ipconfigured');
         });
@@ -193,11 +86,11 @@ function createAccessPoint() {
   });
 
   myEmitter.on('ipconfigured', () => {
-    hostapd({}, () => {
+    APExecs.hostapd({}, () => {
       console.log("Done setting up hostap.");
-      dnsmasq({}, () => {
+      APExecs.dnsmasq({}, () => {
         console.log("Done setting up dnsmasq.");
-        trafficForwarding(() => {
+        APExecs.trafficForwarding(() => {
           console.log("Traffic forwarding setup.");
         });
       });
