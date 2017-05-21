ad6c62719cff4aaaa7479d61839bd44f3459bb75
Matthew Amacker <matt@soovle.com>
2017-05-21 07:04:36 -0700

    Simple bath checker for conditional programming example.

---
+++ python/bath.py
@@ -0,0 +1,11 @@
+name = raw_input('Enter your name: ')
+lastname = raw_input('Enter your lastname: ')
+print("Hello " + name + " " + lastname + "!")
+
+isStinky = raw_input('Are you stinky? (y/n):');
+
+if (isStinky == 'y'):
+  print("Ewwww... Time for a bath!!!");
+else:
+  print("Hooray!!!");
+
