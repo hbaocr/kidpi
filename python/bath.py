ff326ad8d145a148ca23e5e8343fb0416d3edb2b
Matthew Amacker <matt@soovle.com>
2017-05-21 09:13:31 -0700

    Small additions for the lessons.

--- python/bath.py
+++ python/bath.py
@@ -9,3 +9,11 @@ if (isStinky == 'y'):
 else:
   print("Hooray!!!");
 
+hasSoap = raw_input('Do you have soap? (y/n):');
+
+if (hasSoap == 'y'):
+  print("Excellent you are all set!!!");
+else:
+  print("Oh no! Time to go shopping.");
+
+
