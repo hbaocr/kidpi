ae7580a9df794152185dcc1bfeb1e477a4d773c1
BuildTools <unconfigured@null.spigotmc.org>
2017-06-17 22:19:42 -0700

    Bug in creation of pem file.

--- encrypt/createkey.sh
+++ encrypt/createkey.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "This generates the secret.key and the secret.pem."
 echo "Send the secret.key file to your friend."
-echo -e 'y\n'| ssh-keygen -N "" -f secret.key
+echo ""
+echo -e 'y\n'| ssh-keygen -N "" -f secret.key 2>&1 > /dev/null
 ssh-keygen -f secret.key.pub -e -m PKCS8 > secret.pem
-rm secret.key.pub
+echo ""
+echo "Now put the secret in a file named text.txt"
+
+
