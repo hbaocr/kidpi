8cad5c9b7adbe56ac6c9c7467aa9fc81740fb249
BuildTools <unconfigured@null.spigotmc.org>
2017-06-17 22:03:52 -0700

    Added additionall messaging.

--- encrypt/createkey.sh
+++ encrypt/createkey.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-echo -e 'y\n'| ssh-keygen -N "" -f secret
-ssh-keygen -f secret.pub -e -m PKCS8 > secret.pem
+echo "This generates the secret.key and the secret.pem."
+echo "Send the secret.key file to your friend."
+echo -e 'y\n'| ssh-keygen -N "" -f secret.key
+ssh-keygen -f secret.key.pub -e -m PKCS8 > secret.pem
+rm secret.key.pub
--- encrypt/decrypt.sh
+++ encrypt/decrypt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Make sure you've received encrypted.txt and the secret file."
-
-openssl rsautl -inkey secret -decrypt -in encrypted.txt -out results.txt
+echo "Make sure you've received encrypted.txt and the secret.key file."
+openssl rsautl -inkey secret.key -decrypt -in encrypted.txt -out results.txt
+echo "You now have a file called: results.txt"
+echo "That is the secret message."
