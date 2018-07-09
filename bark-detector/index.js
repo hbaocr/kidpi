422142c8068a1b2465d996a51fb8cca97f972532
Matthew Amacker <matt@mattandroxanne.com>
2018-07-09 00:01:05 +0000

    Now with pin tweaking.

--- bark-detector/index.js
+++ bark-detector/index.js
@@ -1,35 +1,61 @@
 const { spawn } = require('child_process');
 var fs = require('fs');
-//rec -q mattpiece.wav sinc 120-2k silence 1 0.1 5% 1 .1 100% : newfile : restart 
-const child = spawn('rec', "-q mattpiece.wav sinc 120-2k silence 1 0.1 5% 1 .1 100% : newfile : restart ".split(" "));
+var child = null;
+function startSpawn() {
+  // Start the soc recorder looking for barks...
+  //rec -q mattpiece.wav sinc 120-2k silence 1 0.1 5% 1 .1 100% : newfile : restart 
+  child = spawn('rec', "-q mattpiece.wav sinc 120-2k silence 1 0.1 5% 1 .1 100% : newfile : restart ".split(" "));
 
-// use child.stdout.setEncoding('utf8'); if you want text chunks
-child.stdout.on('data', (chunk) => {
-  // data from standard output is here as buffers
-  console.log("Out: " + chunk);
-});
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    setTimeout(startSpawn, 1000);
+  });
+}
+startSpawn();
 
-child.stderr.on('data', (chunk) => {
-  // data from standard output is here as buffers
-  console.log("Err chunk: ", " " + chunk);
-});
+function startWork() {
+  fs.watch('.', {}, (eventType, filename) => {
+    if (filename) {
+      console.log("Something changed on: ", filename);
+      if (filename.match(/matt.*wav/)) {
+        console.log("Name matched.");
+        fs.access("./" + filename, fs.constants.F_OK, (err) => {
+          if (!err) {
+            console.log("Got a new bark sound.");
+            fs.unlink("./" + filename);
+            gpio.write(waterPin, false, (err) => {
+              if (err) {
+                console.log("Error in setting gpio pin.",err);
+                return;
+              }
 
-child.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
-
-
-fs.watch('.', {}, (eventType, filename) => {
-  if (filename) {
-    console.log("Something changed on: ", filename);
-    if (filename.match(/matt.*wav/)) {
-      console.log("Name matched.");
-      fs.access("./" + filename, fs.constants.F_OK, (err) => {
-        if (!err) {
-          console.log("Got a new bark sound.");
-          fs.unlink("./" + filename);
-        }
-      })
+              setTimeout(() => {
+                gpio.write(waterPin, true, (err) => {
+                  if (err) {
+                    console.log("Error in setting gpio pin.",err);
+                    return;
+                  }
+                });
+              }, 30000);
+            });
+          }
+        });
+      }
     }
-  }
+  });
+}
+
+var gpio = require('rpi-gpio');
+//gpio.setMode(gpio.MODE_BCM);
+var waterPin = 16;
+gpio.setup(waterPin, gpio.DIR_OUT, () => {
+  startWork();
 });
+
+function exitHandler(ex) {
+  console.log("Whoops:", ex);
+  child.kill();
+}
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler);
