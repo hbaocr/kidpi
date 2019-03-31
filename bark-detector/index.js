79be39b3ff31b5999e3ef0bd78bedf4065f1bd09
Matthew AmackeR <matt@mattandroxanne.com>
2019-03-31 16:15:30 +0000

    Updated handling of recording.

--- bark-detector/index.js
+++ bark-detector/index.js
@@ -1,17 +1,41 @@
 const { spawn } = require('child_process');
 var fs = require('fs');
 var child = null;
-function startSpawn() {
+//const soundsDir = "/tmp/sounds";
+const soundsDir = "/home/pi/kidpi/bark-detector/barks";
+function startSpawn(recTuning) {
   // Start the soc recorder looking for barks...
-  //rec -q mattpiece.wav sinc 120-2k silence 1 0.1 5% 1 .1 100% : newfile : restart 
-  child = spawn('rec', "-q mattpiece.wav sinc 1k-2k silence 1 0.1 5% 1 .1 100% : newfile : restart ".split(" "));
+  //rec -q mattpiece.wav sinc 1k-2k silence 1 0.1 5% 1 .1 100% : newfile : restart 
+  var params
+  if ( child != null ) {
+    child.kill();
+  }
+
+  var recParams = ("-q " + soundsDir+ "/mattpiece" + Math.random() + ".wav sinc 200-2k silence 1 0.1 5% 1 .1 100% : newfile : restart ").split(" ");
+    for(var i in recParams) {
+      console.log(i + ": " +  recParams[i]);
+    }
+  /*
+  recParams[4] = recParams.bottomFreq + "-" + recParams.topFreq;
+  recParams[5] = recParams.numAbovePeriods;
+  recParams[7] = recParams.abovePeriodDuration;
+  */
+
+  child = spawn('rec', recParams);
 
   child.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
+    child = null;
     setTimeout(startSpawn, 1000);
   });
 }
 startSpawn();
+
+// Restart the recording tool every twenty minutes or so.
+setInterval(() => {
+  // Close the rec program. This will cause the "close" event.
+  child.kill();
+}, 1000 * 60 * 20);
 
 // Make sure we reboot every 24 hours
 function rebootComputer() {
@@ -23,15 +47,18 @@ setTimeout(rebootComputer, 1000 * 60 * 60 * 24);
 
 function startWork() {
   console.log("Starting work...");
-  fs.watch('.', {}, (eventType, filename) => {
+  if (!fs.existsSync(soundsDir)){
+    fs.mkdirSync(soundsDir);
+  }
+  fs.watch(soundsDir, {}, (eventType, filename) => {
     if (filename) {
       console.log("Something changed on: ", filename);
       if (filename.match(/matt.*wav/)) {
         console.log("Name matched.");
-        fs.access("./" + filename, fs.constants.F_OK, (err) => {
+        fs.access(soundsDir + "/" + filename, fs.constants.F_OK, (err) => {
           if (!err) {
             console.log("Got a new bark sound.");
-            fs.unlink("./" + filename);
+            fs.unlink(soundsDir + "/" + filename);
             gpio.write(waterPin, true, (err) => {
               if (err) {
                 console.log("Error in setting gpio pin.",err);
