6a389e2e28fe3d7605b9e56af9d4047c1232b30e
Matthew Amacker <matt@soovle.com>
2019-04-13 11:06:15 -0700

    Now with text limiting.

--- backupwarning/index.js
+++ backupwarning/index.js
@@ -6,10 +6,24 @@ var https = require('https');
 
 var app = express()
 
+var lastText = 0;
 app.get('/caron', (req, res) => {
   res.end(JSON.stringify(new Date()));
   http.get('http://192.168.1.63/safety', (res) => {
-    https.get('https://theamackers.com/text?msg=http://192.168.1.63/&password=yesyoumay&n=15304007823', (res) => {});
+    if ((new Date().getTime()) - lastText > 60000 * 5) {
+      https.get('https://theamackers.com/text?msg=' + encodeURIComponent('Safety view - click: http://192.168.1.63/') + '&password=yesyoumay&n=15304001578', (res) => {});
+    }
+    lastText = new Date().getTime();
+  });
+});
+
+app.get('/mattcaron', (req, res) => {
+  res.end(JSON.stringify(new Date()));
+  http.get('http://192.168.1.63/safety', (res) => {
+    if ((new Date().getTime()) - lastText > 60000 * 5) {
+      https.get('https://theamackers.com/text?msg=http://192.168.1.63/&password=yesyoumay&n=15304007823', (res) => {});
+    }
+    lastText = new Date().getTime();
   });
 });
 
