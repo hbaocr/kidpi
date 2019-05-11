e9cd506962531459d14c3a5f8dd7699102530c66
Matthew Amacker <matt@soovle.com>
2019-05-11 15:38:44 -0700

    It worksgit add index.js git add index.js

--- partyline/index.js
+++ partyline/index.js
@@ -3,6 +3,7 @@ var spawn = require('child_process').spawn
 var io = require('socket.io-client')
 var socket = io.connect('https://theamackers.com', {reconnect: true});
 var ss = require('socket.io-stream');
+var EventEmitter = require('events');
 
 socket.on("hi", () => {
   console.log("Server said hi.");
@@ -10,8 +11,9 @@ socket.on("hi", () => {
 });
 
 
-class RecordProcess {
-  constructor() {
+class RecordProcess extends EventEmitter {
+  constructor(pipe) {
+    super();
     let args =  [
       '-c', '1', // 2 channels
       '-r', '44100', // 44100Hz sample rate
@@ -20,36 +22,54 @@ class RecordProcess {
     ]
     console.log("Spawning: arecord ", args.join(" "));
     this.process = spawn('arecord', args);
+    this.process.stdout.pipe(pipe);
+
     this.process.once('exit', (code) => {
       console.log("Record process ended.", code);
+      this.emit("exit");
     });
   }
 }
 
-class AplayProcess {
-  constructor() {
+class AplayProcess extends EventEmitter {
+  constructor(pipe) {
+    super();
     let args =  [ ]
     console.log("Spawning: aplay ", args.join(" "));
     this.process = spawn('aplay', args);
+    pipe.pipe(this.process.stdin);
+
     this.process.once('exit', (code) => {
       console.log("Play process ended.", code);
+      this.emit("exit");
     });
   }
 }
 
-let rec = new RecordProcess().process.stdout;
-let play = new AplayProcess().process.stdin;
+function createRecPipe(pipe) {
+  let rec = new RecordProcess(pipe);
+  rec.on("exit", () => {
+    createRecPipe(pipe);
+  });
+}
+
+function createPlayPipe(pipe) {
+  let play = new AplayProcess(pipe);
+  play.on("exit", () => {
+    createPlayPipe(pipe);
+  });
+}
 
 function startPiping() {
   ss(socket).on("party-audio", (stream) => {
     console.log("Got an audio stream...");
-    stream.pipe(play);
+    createPlayPipe(stream);
   });
 
   console.log("Piping.");
-
   let outStream = ss.createStream();
-  rec.pipe(outStream);
+  createRecPipe(outStream);
+
   ss(socket).emit("audio-stream", outStream, {name: "mamacker"});
   console.log("Started piping...");
 }
