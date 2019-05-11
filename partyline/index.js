1d01b2a2eb774e99d2616f438cd072d43a980d36
mamacker <matt@soovle.com>
2019-05-11 22:15:49 +0000

    Now using raw processes.

--- partyline/index.js
+++ partyline/index.js
@@ -4,29 +4,11 @@ var io = require('socket.io-client')
 var socket = io.connect('https://theamackers.com', {reconnect: true});
 var ss = require('socket.io-stream');
 
-
 socket.on("hi", () => {
   console.log("Server said hi.");
+  startPiping();
 });
 
-
-// Creates wrapper on record process.
-class Microphone {
-  constructor(onDone) {
-    this.ready = false;
-    this.rec = new RecordProcess(() => {
-      this.ready = true;
-      onDone();
-    });
-
-    this.stdout = this.rec.stdout;
-  }
-
-  pipe(toPipe) {
-    console.log("Piping output through.");
-    this.stdout.pipe(toPipe);
-  }
-}
 
 class RecordProcess {
   constructor() {
@@ -44,19 +26,31 @@ class RecordProcess {
   }
 }
 
-//let mic = require('mic-stream');
-let mic = new Microphone(() => {
-  //https://stackoverflow.com/questions/24071561/how-to-send-binary-data-from-a-node-js-socket-io-server-to-a-browser-client
-  //mic.pipe(speaker());
-  let outStream = ss.createStream();
-  ss(socket).emit("audio-stream", outStream);
-  mic().pipe(outStream);
-});
-//
+class AplayProcess {
+  constructor() {
+    let args =  [ ]
+    console.log("Spawning: aplay ", args.join(" "));
+    this.process = spawn('aplay', args);
+    this.process.once('exit', (code) => {
+      console.log("Play process ended.", code);
+    });
+  }
+}
 
-let speaker = require('audio-speaker');
-let output = speaker();
-ss(socket).on("audio-stream", (stream) => {
-  stream.pipe(output);
-});
+let rec = new RecordProcess().process.stdout;
+let play = new AplayProcess().process.stdin;
+
+function startPiping() {
+  ss(socket).on("party-audio", (stream) => {
+    console.log("Got an audio stream...");
+    stream.pipe(play);
+  });
+
+  console.log("Piping.");
+
+  let outStream = ss.createStream();
+  rec.pipe(outStream);
+  ss(socket).emit("audio-stream", outStream, {name: "mamacker"});
+  console.log("Started piping...");
+}
 
